cc9b530acf4ecb1d9235afd253bcfa66fcc0b3ea
Agniiiiiman <agniv0499@gmail.com>
2025-07-09 12:51:40 +0530

    ADDED A NEW STRING 1.CONCATENATION STRING 2.ESCAPE STRING 3.FORMAT STRING 4.MODIFYING STRING 5.SLICING STRING 6.STRING METHOD

---
+++ STRING/ESCAPEstring.py
@@ -0,0 +1,17 @@
+"""
+Escape Character
+To insert characters that are illegal in a string, use an escape character.
+
+An escape character is a backslash \ followed by the character you want to insert.
+
+An example of an illegal character is a double quote inside a string that is surrounded by double quotes:
+
+"""
+
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+print("She said \"Hello!\"")
+print('It\'s a beautiful day.')
+print("This is a new line\nThis is the next line.")
+print("Tab\tseparated\twords")
