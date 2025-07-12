b6114e949a274179c900cddea193350b24b55800
Agniiiiiman <agniv0499@gmail.com>
2025-07-12 13:07:09 +0530

    Match switch

---
+++ MATCH (SWITCH)/Combined_value.py
@@ -0,0 +1,7 @@
+day=4
+match day :
+    case 1| 2 | 3 | 4 | 5:
+        print("TODAY IS A WEEKEND")
+    case 6|7 :
+        print("I LOVE WEEKEND ")  
+          
---
+++ MATCH (SWITCH)/weekend.py
@@ -0,0 +1,18 @@
+day =4
+match day :
+    case 1:
+        print("MONDAY")
+    case 2:
+        print("TUESDAY")
+    case 3:
+        print("WEDNESDAY")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+                           
+    
